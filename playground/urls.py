87cae72adf2be7719027b0792398548e4414f151
SundarDong <brosisstudio123@gmail.com>
2025-01-19 20:56:06 +0545

    feat: learning Mapping Urls to view

---
+++ playground/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+#Url Configuration
+#mapping Url function to views
+urlpatterns= [
+    path('hello/', views.say_hello)
+]
--- playground/views.py
+++ playground/views.py
@@ -1,1 +1,5 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+
+def say_hello(request):
+    return HttpResponse("Hello World")
